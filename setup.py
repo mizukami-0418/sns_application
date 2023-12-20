7e262a00686751c9d2057e8f2c453e36c8b36d60
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2023-12-20 21:22:31 +0900

    setup.pyを編集し、エントリーポイントを作成

---
+++ setup.py
@@ -0,0 +1,8 @@
+# setup.py
+# Flaskアプリのエントリーポイント
+from flaskr import create_app
+# create_app()関数を呼び出し、Flaskアプリのインスタンスを作成、変数appに割当
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
