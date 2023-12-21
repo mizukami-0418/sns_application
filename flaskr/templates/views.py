483bab753577180d1855fd3ff4979ef1f72bb16a
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2023-12-21 20:41:43 +0900

    views.pyの編集。ログインのLoginFormまで

---
+++ flaskr/templates/views.py
@@ -0,0 +1,29 @@
+# views.py
+from flask import (
+    Blueprint, abort, request, render_template,
+    redirect, url_for, flash
+)
+from flask_login import login_user, login_required, logout_user
+from flaskr.models import (User, PasswordResetToken)
+from flaskr import db
+
+from os import path
+
+# Blueprintの作成
+bp = Blueprint("app", __name__, url_prefix="")
+
+# ホーム画面
+@bp.route("/")
+def home():
+    return render_template("home.html")
+
+# ログアウト
+@bp.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("app.home")) # ログアウトしたらホーム画面へ
+
+# ログイン
+@bp.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm(request.form)
