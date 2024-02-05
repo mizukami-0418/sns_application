727af9502c66bc513a1634c359410fef166ac201
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2024-02-05 23:14:14 +0900

    models.py#UserConnectクラスにis_friendメソッドを追加 Messageクラスを作成し、2種のメソッドを定義

--- flaskr/models.py
+++ flaskr/models.py
@@ -396,3 +396,61 @@ class UserConnect(db.Model):
   def update_status(self):
     self.status = 2
     self.update_at = datetime.now()
+    
+  @classmethod
+  def is_friend(cls, to_user_id):
+    user = cls.query.filter(
+      or_(
+        and_(
+          UserConnect.from_user_id == current_user.get_id(),
+          UserConnect.to_user_id == to_user_id,
+          UserConnect.status == 2
+        ),
+        and_(
+          UserConnect.from_user_id == to_user_id,
+          UserConnect.to_user_id == current_user.get_id(),
+          UserConnect.status == 2
+        )
+      )
+    ).first()
+    return True if user else False
+
+class Message(db.Model):
+  
+  __tablename__ = 'messages'
+  
+  id = db.Column(db.Integer, primary_key=True)
+  from_user_id = db.Column(
+    db.Integer, db.ForeignKey('users.id'), index=True
+  )
+  id = db.Column(db.Integer, primary_key=True)
+  to_user_id = db.Column(
+    db.Integer, db.ForeignKey('users.id'), index=True
+  )
+  is_read = db.Column(db.Boolean, default=False,)
+  message = db.Column(db.Text)
+  create_at = db.Column(db.DateTime, default=datetime.now)
+  update_at = db.Column(db.DateTime, default=datetime.now)
+  
+  def __init__(self, from_user_id, to_user_id, message):
+    self.from_user_id = from_user_id
+    self.to_user_id = to_user_id
+    self.message = message
+    
+  def create_message(self):
+    db.session.add(self)
+    
+  @classmethod
+  def get_friend_messages(cls, id1, id2):
+    return cls.query.filter(
+      or_(
+        and_(
+          cls.from_user_id == id1,
+          cls.to_user_id == id2
+        ),
+        and_(
+          cls.from_user_id == id2,
+          cls.to_user_id == id1
+        )
+      )
+    ).order_by(cls.id).all()
